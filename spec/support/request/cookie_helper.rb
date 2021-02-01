75655be195a1b74b2ac9a1aca65b2395c9862180
Arun Kumar Mohan <arunmohandm@gmail.com>
2021-02-01 03:16:10 -0500

    Fix no such cookie error

--- spec/support/request/cookie_helper.rb
+++ spec/support/request/cookie_helper.rb
@@ -3,5 +3,7 @@
 module CookieHelper
   def cookie_named(name)
     Capybara.current_session.driver.browser.manage.cookie_named(name)
+  rescue Selenium::WebDriver::Error::NoSuchCookieError
+    nil
   end
 end
